d85c661a6fc1742b3897c91ca3d02fe8859a7c3c
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-01-13 15:43:34 +0100

    page d'accueil ready

--- nodejswebshop/app/server.js
+++ nodejswebshop/app/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
 const app = express();
+
 const userRoute = require("./routes/User");
+const homeRoute = require("./routes/accueil");
+
 app.use("/user", userRoute);
+app.use("/accueil", homeRoute);
 
 // Démarrage du serveur
 app.listen(8080, () => {
