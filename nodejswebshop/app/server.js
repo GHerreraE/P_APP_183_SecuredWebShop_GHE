9046c58c1a1bfdfbdc3d7110a2fb023d720dee94
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-01-24 11:40:36 +0100

    Login + register = ready

--- nodejswebshop/app/server.js
+++ nodejswebshop/app/server.js
@@ -1,10 +1,28 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
 
+// dossier pour les fichiers statiques
+app.use(express.static(path.join(__dirname, "public")));
+
+// config du motoeur de template EJS
+app.set("view engine", "ejs");
+
+// routes principales
+app.get("/login", (req, res) => {
+  res.render("login", { name: "Gonzalo" });
+});
+
+app.post(".");
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// importation de routes
 const userRoute = require("./routes/User");
 const homeRoute = require("./routes/accueil");
 
+// routes
 app.use("/user", userRoute);
 app.use("/accueil", homeRoute);
 
