3cb01fcb5f1c0ea9572952f973b69d251a689911
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-03-11 10:55:49 +0100

    feat(routes): Nouvelle route /admin (get/post) + Modification /profile -> /users + Get /users/:username

--- nodejswebshop/app/routes/User.js
+++ nodejswebshop/app/routes/User.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const registerController = require("../controllers/registerController");
 const loginController = require("../controllers/loginController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const adminController = require("../controllers/adminController");
+const connection = require("../db/connectionDb");
 
 // cookie-parser => packet npm
 
@@ -30,11 +32,7 @@ router.get("/postRegister", (req, res) => {
 
 // Route protégée : Affiche le dashboard uniquement si l'utilisateur est connecté
 router.get("/dashboard", authMiddleware, (req, res) => {
-  res.render("dashboard", { username: req.user.username });
-});
-
-router.get("/users", authMiddleware, (req, res) => {
-  res.redirect("/users" + req.user.username);
+  res.render("dashboard", { user: req.user });
 });
 
 router.get("/logout", (req, res) => {
@@ -43,13 +41,31 @@ router.get("/logout", (req, res) => {
   res.redirect("/login");
 });
 
+// Route protégée : Affiche les détails de l'utilisateur connecté
+router.get("/users", authMiddleware, (req, res) => {
+  res.redirect(`/users/${req.user.username}`);
+});
+
 // Route protégée : Affiche le profil de l'utilisateur connecté
-router.get("/profile", authMiddleware, (req, res) => {
-  res.render("profile", {
-    username: req.user.username,
-    role: req.user.role,
+router.get("/users/:username", authMiddleware, (req, res) => {
+  const username = req.params.username;
+
+  const query = "SELECT id, username, role FROM t_users WHERE username = ?";
+  connection.query(query, [username], (err, results) => {
+    if (err) {
+      console.error("Erreur SQL :", err);
+      return res.status(500).send("Erreur serveur");
+    }
+
+    if (results.length === 0) {
+      return res.status(404).send("Utilisateur non trouvé");
+    }
+
+    res.render("users", { user: results[0] });
   });
 });
+
+router.get("/admin", authMiddleware, adminController.adminPage);
 
 /*********************** ROUTES POST ***********************/
 
@@ -59,4 +75,6 @@ router.post("/register", registerController.registerUser);
 // Route POST pour la connexion
 router.post("/login", loginController.loginUser);
 
+// Route POST pour l'admin page
+router.post("/admin", authMiddleware, adminController.searchUsers);
 module.exports = router;
