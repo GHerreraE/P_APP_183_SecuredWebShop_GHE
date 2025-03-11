a5d58685009a33c058985195f8a84b6408ef4ff4
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-03-11 10:34:11 +0100

    feat(connectionDB) : Remarquer avec un message que c'est pour la connection

--- nodejswebshop/app/db/connectionDb.js
+++ nodejswebshop/app/db/connectionDb.js
@@ -1,6 +1,5 @@
 const mysql = require("mysql2");
 
-// Configuration de la connexion MySQL
 const connection = mysql.createConnection({
   host: "db_container",
   user: "root",
@@ -9,14 +8,14 @@ const connection = mysql.createConnection({
   database: "db_webshop",
 });
 
-// Connexion à la base de données
 connection.connect((err) => {
   if (err) {
     console.error("Erreur de connexion à MySQL :", err.stack);
     return;
   }
-  console.log("Connecté à la base de données MySQL !");
+  console.log(
+    "Connecté à la base de données MySQL ! -> Pret pour les requetes"
+  );
 });
 
-// Export de la connexion pour l’utiliser ailleurs
 module.exports = connection;
